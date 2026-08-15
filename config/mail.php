6b2a09ab559a384b5b4ac619a63c96eea7d67dd0
Fajar Arena <fajararenabadminton@gmail.com>
2026-08-15 15:53:34 +0800

    fix: allow SSL stream options in mail configuration to prevent SMTP proxy interception

--- config/mail.php
+++ config/mail.php
@@ -44,6 +44,14 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'scheme' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
