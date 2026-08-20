b5e5f678700d9bc9f838b20c2f7818df83c87df2
Fajar Arena <fajararenabadminton@gmail.com>
2026-08-20 13:09:29 +0800

    Disable STARTTLS peer verification on SocketStream for cPanel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,25 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production') || (config('app.url') && str_starts_with(config('app.url'), 'https://'))) {
             URL::forceScheme('https');
         }
+
+        // Override SMTP transport to disable SSL/STARTTLS peer certificate checks on cPanel
+        Mail::extend('smtp', function (array $config = []) {
+            $transport = new EsmtpTransport('smtp.gmail.com', 587, false);
+            $transport->setUsername('fajararenabadminton@gmail.com');
+            $transport->setPassword('kczztgbfghwfqafa');
+
+            $stream = $transport->getStream();
+            if ($stream instanceof SocketStream) {
+                $stream->setStreamOptions([
+                    'ssl' => [
+                        'allow_self_signed' => true,
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                    ],
+                ]);
+            }
+
+            return $transport;
+        });
     }
 }
